0a3562f57257e25e948c55610f3212f0a0ed479a
Douglas Mason <dmason@truefitsolutions.com>
2013-03-05 10:02:27 -0500

    adds player profiles

--- tropical/Controllers/TeamController.cs
+++ tropical/Controllers/TeamController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
 using System.Web;
 using System.Web.Mvc;
+using tropical.Models.ViewModels;
 
 namespace tropical.Controllers
 {
@@ -13,8 +15,31 @@ namespace tropical.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var players = GetTeamPlayers();
+
+            return View(players);
         }
 
+        private IEnumerable<PlayerView> GetTeamPlayers()
+        {
+            var xmlDoc = new XmlDocument();
+            var playerList = new List<PlayerView>();
+            var path = Server.MapPath("/Xml/Team.xml");
+            xmlDoc.Load(path);
+            var playerDocItems = xmlDoc.GetElementsByTagName("Player");
+            foreach (XmlNode item in playerDocItems)
+            {
+                var player = new PlayerView();
+                player.FirstName = item["FirstName"] != null ? item["FirstName"].InnerText : "";
+                player.LastName = item["LastName"] != null ? item["LastName"].InnerText : "";
+                player.Grade = item["Grade"] != null ? item["Grade"].InnerText : "";
+                player.Position = item["Position"] != null ? item["Position"].InnerText : "";
+                player.Height = item["Height"] != null ? item["Height"].InnerText : "";
+                player.Quote = item["Quote"] != null ? item["Quote"].InnerText : "";
+                player.JerseyNumber = item["JerseyNumber"] != null ? Int32.Parse(item["JerseyNumber"].InnerText) : 0;
+                playerList.Add(player);
+            }
+            return playerList;
+        }
     }
 }
---
+++ tropical/Models/ViewModels/PlayerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tropical.Models.ViewModels
+{
+    public class PlayerView
+    {
+        public int JerseyNumber { get; set; }  // Will also serve as jersey number which is unique
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public string Grade { get; set; }
+        public string Height { get; set; }
+        public string Quote { get; set; }
+    }
+}
