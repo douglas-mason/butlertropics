6e4d649e6ae0dc33e2ff2d3c84f3e32f1f77f76d
Douglas Mason <dmason@truefitsolutions.com>
2013-03-12 21:05:48 -0400

    adds schedule views, controllers, and data

--- tropical/Controllers/ScheduleController.cs
+++ tropical/Controllers/ScheduleController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using tropical.Models.ViewModels;
 
 namespace tropical.Controllers
 {
@@ -13,8 +15,33 @@ namespace tropical.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var schedule = GetSchedule();
+            return View(schedule);
         }
 
+        public ActionResult EventDetail(int eventId)
+        {
+            var scheduledEvent = new ScheduleView();
+            return View(scheduledEvent);
+        }
+
+        private IEnumerable<ScheduleView> GetSchedule()
+        {
+            var xmlDoc = new XmlDocument();
+            var scheduleList = new List<ScheduleView>();
+            var path = Server.MapPath("/Xml/Schedule.xml");
+            xmlDoc.Load(path);
+            var playerDocItems = xmlDoc.GetElementsByTagName("Event");
+            foreach (XmlNode item in playerDocItems)
+            {
+                var scheduledEvent = new ScheduleView();
+                scheduledEvent.Id = item["Id"] != null ? int.Parse(item["Id"].InnerText) : 0;
+                scheduledEvent.Dates = item["Dates"] != null ? item["Dates"].InnerText : "";
+                scheduledEvent.Tournament = item["Tournament"] != null ? item["Tournament"].InnerText : "";
+                scheduledEvent.Location = item["Location"] != null ? item["Location"].InnerText : "";
+                scheduleList.Add(scheduledEvent);
+            }
+            return scheduleList;
+        }
     }
 }
---
+++ tropical/Models/ViewModels/ScheduleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tropical.Models.ViewModels
+{
+    public class ScheduleView
+    {
+        public int Id { get; set; }
+        public string Dates { get; set; }
+        public string Tournament { get; set; }
+        public string Location { get; set; }
+    }
+}
